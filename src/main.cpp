56970d823dbbe6717f78f53102dbb786460b8df1
Seno Pamungkas <senopamungkasraehman@gmail.com>
2025-08-21 00:09:46 +0700

    feat: YAML projects parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,82 @@
 #include <iostream>
 #include "yaml-cpp/yaml.h"
 
-struct Employee
+struct Project
 {
-    int id {};
-    int age {};
-    double wage {};
+    std::string name {};
+    std::string github {};
+    std::string github_handle {};
+    std::vector<std::string> cv_type {};
+    std::string description {};
+    std::vector<std::string> points {};
+
+    Project() = default;
+
+    // Constructor
+    explicit Project(std::string name,
+        std::string github,
+        std::string github_handle,
+        std::vector<std::string> cv_type,
+        std::string description,
+        std::vector<std::string> points) :
+      name(name),
+      github(github),
+      github_handle(github_handle),
+      cv_type(cv_type),
+      description(description),
+      points(points) {}
+
+    friend std::ostream& operator<<(std::ostream& os, const Project& p);
 };
 
-
-std::ostream& operator<<(std::ostream& os, const Employee& e) {
-    os << "Employee { id: " << e.id
-       << ", age: " << e.age
-       << ", wage: " << e.wage << " }";
+std::ostream& operator<<(std::ostream& os, const Project& p) {
+    os << "Project { name: " << p.name << '\n'
+       << "github: " << p.github << '\n'
+       << "github_handle: " << p.github_handle << '\n'
+       << "cv_type: [ " ;
+        for (const auto& type : p.cv_type){
+          os << type << ", ";
+        }
+       os << " ]\n"
+       << "description: " << p.description << '\n'
+       << "points: [ " ;
+        for (const auto& point : p.points){
+          os << point << "\n";
+        }
+       os << "]"  << " }";
     return os;
 }
 
 int main(){
+  std::vector<Project> projects;
+
     try {
-        // Load the YAML file into a root node.
-        // This line does all the parsing.
-        YAML::Node config = YAML::LoadFile("config.yaml");
+        YAML::Node projects_node = YAML::LoadFile("config/projects.yaml");
 
-        // --- Accessing Map Values ---
-        // You can chain accessors for nested maps.
-        std::string appName = config["application"]["name"].as<std::string>();
-        double appVersion = config["application"]["version"].as<double>();
-        int dbPort = config["database"]["port"].as<int>();
+        if (projects_node && projects_node.IsSequence()) {
+            Project project;
+            for (const YAML::Node& project_node : projects_node) {
+                std::vector<std::string> cv_types ;
+                if (project_node["cv_type"] && project_node["cv_type"].IsSequence()) {
+                    cv_types = project_node["cv_type"].as<std::vector<std::string>>();
+                }
+                project.name = project_node["name"].as<std::string>();
+                project.github =  project_node["github"].as<std::string>();
+                project.github_handle =  project_node["github_handle"].as<std::string>();
+                project.cv_type =  std::move(cv_types);
+                project.description =  project_node["description"].as<std::string>();
+                project.points =  project_node["points"].as<std::vector<std::string>>();
 
-        std::cout << "Application Name: " << appName << std::endl;
-        std::cout << "Application Version: " << appVersion << std::endl;
-        std::cout << "Database Port: " << dbPort << std::endl;
+                std::cout << "- " << project << "\n\n";
 
-        // --- Checking for Optional Keys ---
-        // Always check if a node exists before accessing it.
-        if (config["application"]["threaded"]) {
-            bool isThreaded = config["application"]["threaded"].as<bool>();
-            std::cout << "Threading enabled: " << (isThreaded ? "true" : "false") << std::endl;
-        }
-
-        // --- Iterating Over a Sequence (List) ---
-        const YAML::Node& users = config["database"]["users"];
-        if (users && users.IsSequence()) {
-            std::cout << "Database Users:" << std::endl;
-            for (const YAML::Node& user : users) {
-                std::cout << "- " << user.as<std::string>() << std::endl;
+                projects.emplace_back(std::move(project));
             }
         }
+        std::cout << "Total Project: " << projects.size() << '\n';
 
     } catch (const YAML::Exception& e) {
-        // Handle file not found, parsing errors, etc.
         std::cerr << "Error parsing YAML: " << e.what() << std::endl;
-        return 1;
+        std::exit(1);
     }
   return 0;
 }
